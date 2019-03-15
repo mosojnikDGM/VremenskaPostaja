748211cf6c54f4dc9683bba6941baa2e17c6c803
Mitja Osojnik <mitja.osojnik@druga.si>
2019-03-15 14:31:05 +0100

    Includes

--- VremenskaPostaja/VremenskaPostaja.ino
+++ VremenskaPostaja/VremenskaPostaja.ino
@@ -1,9 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "Arduino.h"
+#include "heltec.h"
+#include "images.h"
+#include<SPI.h>
+#include<Wire.h>  //responsible for i2c communication
 
+void setup() {
+  Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Disable*/, true /*Serial Enable*/);
+
+  Heltec.display->flipScreenVertically();
+  Heltec.display->setFont(ArialMT_Plain_10);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Heltec.display->clear();
 
+  Heltec.display->setTextAlignment(TEXT_ALIGN_CENTER);
+  Heltec.display->drawString(64, 22, "Hello World!");
+  Heltec.display->display();
 }
